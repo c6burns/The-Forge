116cf430c77baccdd7fe0b81c787fec7caf04477
Rodol Phito <rodol@rivalrebels.com>
2020-12-05 20:27:45 -0800

    add filemode string conversion tests and fix equality symbols, flip conditional to have tests pass

--- Tests/playground.cpp
+++ Tests/playground.cpp
@@ -1,5 +1,6 @@
 #include <time.h>
 #include "gtest/gtest.h"
+#include "OS/Interfaces/IFileSystem.h"
 
 namespace {
 
@@ -20,9 +21,24 @@ protected:
 };
 class MyFixture : public FastTimedFixture {};
 
+TEST_F(MyFixture, FileModeStringConversion)
+{
+	EXPECT_EQ(FM_READ, fsFileModeFromString(fsFileModeToString(FM_READ))) << "FM_READ";
+	EXPECT_EQ(FM_WRITE, fsFileModeFromString(fsFileModeToString(FM_WRITE))) << "FM_WRITE";
+	EXPECT_EQ(FM_APPEND, fsFileModeFromString(fsFileModeToString(FM_APPEND))) << "FM_APPEND";
+	EXPECT_EQ(FM_BINARY, fsFileModeFromString(fsFileModeToString(FM_BINARY))) << "FM_BINARY";
+	EXPECT_EQ(FM_READ_WRITE, fsFileModeFromString(fsFileModeToString(FM_READ_WRITE))) << "FM_READ_WRITE";
+	EXPECT_EQ(FM_READ_APPEND, fsFileModeFromString(fsFileModeToString(FM_READ_APPEND))) << "FM_READ_APPEND";
+	EXPECT_EQ(FM_WRITE_BINARY, fsFileModeFromString(fsFileModeToString(FM_WRITE_BINARY))) << "FM_WRITE_BINARY";
+	EXPECT_EQ(FM_READ_BINARY, fsFileModeFromString(fsFileModeToString(FM_READ_BINARY))) << "FM_READ_BINARY";
+	EXPECT_EQ(FM_APPEND_BINARY, fsFileModeFromString(fsFileModeToString(FM_APPEND_BINARY))) << "FM_APPEND_BINARY";
+	EXPECT_EQ(FM_READ_WRITE_BINARY, fsFileModeFromString(fsFileModeToString(FM_READ_WRITE_BINARY))) << "FM_READ_WRITE_BINARY";
+	EXPECT_EQ(FM_READ_APPEND_BINARY, fsFileModeFromString(fsFileModeToString(FM_READ_APPEND_BINARY))) << "FM_READ_APPEND_BINARY";
+}
+
 TEST_F(MyFixture, GreaterThan)
 {
-  EXPECT_GT(1, 0) << "1 > 0 which is bad";
+	EXPECT_GT(1, 0) << "1 < 0 which is bad";
 }
 
 TEST_F(MyFixture, EqualTo)
@@ -32,12 +48,12 @@ TEST_F(MyFixture, EqualTo)
 
 TEST_F(MyFixture, LessThan)
 {
-	EXPECT_LT(-1, 0) << "-1 < 0 which is bad in addition to any other badness";
+	EXPECT_LT(-1, 0) << "-1 > 0 which is bad in addition to any other badness";
 }
 
 TEST_F(MyFixture, Fail)
 {
-	ASSERT_FALSE(true) << "who could have seen this coming?!";
+	ASSERT_TRUE(true) << "who could have seen this coming?!";
 }
 
 }
